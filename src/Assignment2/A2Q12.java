10cb3c43d8e0d2309a211ec3a8a1f2864e2e44d5
shivamvk <shivam.vk529@gmail.com>
2018-09-01 10:49:05 +0530

    Solutions till assignment 2

--- src/Assignment2/A2Q12.java
+++ src/Assignment2/A2Q12.java
@@ -6,23 +6,33 @@ public class A2Q12 {
     public static void main(String ar[]){
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int gap = n-1;
-        for (int i=1; i<=n; i++){
-            int k = i;
-            for (int j=0; j<gap; j++){
+        int gap = 1;
+        for (int i=1; i<=n/2+1; i++){
+            for (int j=1; j<=n/2+2-i; j++){
+                System.out.print("*");
+            }
+            for (int j=1; j<=gap; j++){
                 System.out.print(" ");
             }
-            for (int j=1; j<=i; j++){
-                System.out.print(k);
-                k++;
-            }
-            k-=2;
-            for (int l=1; l<i; l++){
-                System.out.print(k);
-                k--;
+            for (int j=1; j<=n/2+2-i; j++){
+                System.out.print("*");
             }
             System.out.println();
-            gap--;
+            gap+=2;
+        }
+        gap-=3;
+        for (int i=1; i<=n/2; i++){
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            for (int j=1; j<gap; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i+1; j++){
+                System.out.print("*");
+            }
+            gap-=2;
+            System.out.println();
         }
     }
 }
